6701c4e5b7de572430bc1bc2f08c9e10ace53d92
Toshiaki Ocho <bigbird19980311@gmail.com>
2021-07-07 15:15:26 +0900

    中間テーブルとしてRentalモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_050056) do
+ActiveRecord::Schema.define(version: 2021_07_07_060827) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2021_07_07_050056) do
     t.text "summary"
   end
 
+  create_table "rentals", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_rentals_on_book_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,4 +52,6 @@ ActiveRecord::Schema.define(version: 2021_07_07_050056) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rentals", "books"
+  add_foreign_key "rentals", "users"
 end
---
+++ test/models/rental_test.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: rentals
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  book_id    :integer          not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_rentals_on_book_id  (book_id)
+#  index_rentals_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  book_id  (book_id => books.id)
+#  user_id  (user_id => users.id)
+#
+require "test_helper"
+
+class RentalTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
